7927a33d1988ec869fe8c1e148778956e547e0ee
Mykola Fedotov <fedotovns@gmail.com>
2024-05-30 10:07:32 +0200

    fixed path error

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(process.cwd(), "public")));
+app.use(express.static("public"));
 
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
